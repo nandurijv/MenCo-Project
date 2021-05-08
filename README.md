11e5039107a6e77239ca56d89a92b3fb5b3cabe1
Nanduri Jayant Vishnu <76738417+nandurijv@users.noreply.github.com>
2021-05-08 18:53:42 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# MenCo-Project
+# MenCo
+### Team Members👨🏻‍💻:
+1. Alok Mathur
+2. Jayant Vishnu Nanduri
+3. Prasoon Soni
+
+### Introduction ✅:
+<p>
+ 
+</p>
+
+### Problem Solution💡:
+<p>
+ 
+</p>
+
+### Novelty📕:
+<p>
+  
+</p>
+
+### Tech Stack 📚:
+- Android Studio
+- Figma
+- Firebase
+- HTML5
+- CSS3
+- JavaScript
+
+### Additional Features🆕:
+<p>
+
+</p>
+
+### Conclusion 🔍:
+<p>
+  
+</p>
---
+++ js/script.js
@@ -0,0 +1,29 @@
+const navbar = document.getElementById("navbar");
+const navbarToggle = navbar.querySelector(".navbar-toggle");
+
+function openMobileNavbar() {
+  navbar.classList.add("opened");
+  navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+}
+
+function closeMobileNavbar() {
+  navbar.classList.remove("opened");
+  navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+}
+
+navbarToggle.addEventListener("click", () => {
+  if (navbar.classList.contains("opened")) {
+    closeMobileNavbar();
+  } else {
+    openMobileNavbar();
+  }
+});
+
+const navbarMenu = navbar.querySelector(".navbar-menu");
+const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+navbarLinksContainer.addEventListener("click", (clickEvent) => {
+  clickEvent.stopPropagation();
+});
+
+navbarMenu.addEventListener("click", closeMobileNavbar);
